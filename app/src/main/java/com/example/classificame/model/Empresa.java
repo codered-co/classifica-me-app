4432394c0fb841898c10971d66f542fa864e0876
RodrigoBP99 <rodrigobozapontes@gmail.com>
2019-08-12 16:07:59 -0300

    Cria adapter para Rankin 	Cria metodo para trocar icone de acordo com ranking da empresa

--- app/src/main/java/com/example/classificame/model/Empresa.java
+++ app/src/main/java/com/example/classificame/model/Empresa.java
@@ -3,6 +3,8 @@ package com.example.classificame.model;
 public class Empresa {
 
     private String nomeEmpresa, descricaoEmpresa, localEmpresa, categoriaEmpresa, imagemEmpresa, tipoEmpresa;
+    private float atendimentoCliente, formaPagamento, servicoEntrega, possibilidadeVoltar, classificacaoEmpresa;
+    private int votosEmpresa;
 
     public Empresa() {
         this.nomeEmpresa = nomeEmpresa;
@@ -59,5 +61,53 @@ public class Empresa {
 
     public void setTipoEmpresa(String tipoEmpresa) {
         this.tipoEmpresa = tipoEmpresa;
+    }
+
+    public float getAtendimentoCliente() {
+        return atendimentoCliente;
+    }
+
+    public void setAtendimentoCliente(float atendimentoCliente) {
+        this.atendimentoCliente = atendimentoCliente;
+    }
+
+    public float getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(float formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public float getServicoEntrega() {
+        return servicoEntrega;
+    }
+
+    public void setServicoEntrega(float servicoEntrega) {
+        this.servicoEntrega = servicoEntrega;
+    }
+
+    public float getPossibilidadeVoltar() {
+        return possibilidadeVoltar;
+    }
+
+    public void setPossibilidadeVoltar(float possibilidadeVoltar) {
+        this.possibilidadeVoltar = possibilidadeVoltar;
+    }
+
+    public float getClassificacaoEmpresa() {
+        return classificacaoEmpresa;
+    }
+
+    public void setClassificacaoEmpresa(float classificacaoEmpresa) {
+        this.classificacaoEmpresa = classificacaoEmpresa;
+    }
+
+    public int getVotosEmpresa() {
+        return votosEmpresa;
+    }
+
+    public void setVotosEmpresa(int votosEmpresa) {
+        this.votosEmpresa = votosEmpresa;
     }
 }
