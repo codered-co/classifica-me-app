4195a3a3e6bec7401537b5cc1ee1ec0fd02f6707
Ivan Viana <ivan.viana@live.com>
2019-08-21 22:47:56 -0300

    Recuperando empresas no ClassificarFragment

--- app/src/main/java/com/example/classificame/fragment/RankingFragment.java
+++ app/src/main/java/com/example/classificame/fragment/RankingFragment.java
@@ -1,9 +1,7 @@
 package com.example.classificame.fragment;
 
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -36,7 +34,6 @@ public class RankingFragment extends Fragment {
     private Empresa empresa;
 
     public RankingFragment() { }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
